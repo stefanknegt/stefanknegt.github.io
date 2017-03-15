c76d2e75abd169d7d1803503ce397f76f1005365
stefanknegt <stefanknegt@gmail.com>
2017-03-15 21:13:20 +0100

    First implementation of javascript working

--- js/main.js
+++ js/main.js
@@ -5,24 +5,84 @@ $('ul.nav').find('a').click(function(){
   return false;
 })
 
-$('#announcement1').on('click',function() {
+generalknowledge = [];
+beliefsusan = [];
+beliefnormal = [];
+belieflost = [];
 
-  // Debug
-  var test = $('#agent1').text();
-  console.log("De waarde is ", test);
+// Given the current knowledge, determine the implications
+function checkImplications(array) {
+  var changed = 1;
 
-  if($('#agent1').text() == 'Test') {
-    $('#agent1').html('Hoi');
+  while(changed == 1) {
+
+    if(array.includes("gr")) {
+      beliefnormal.push("dr")
+    } else if(array.includes("ir") && !array.includes("gr")) {
+      alert("there is only dangerous radiation for normal robots present");
+    }
+      changed = 0;
+    }
+};
+
+// Print all the formulas for the agents and the general knowledge
+function generateModel(array,agent) {
+  var output = [];
+  for (var i = 0; i < array.length; i++) {
+    output.push('<span>' + array[i] + '</span>');
+  }
+  $('#'+agent).html(output.join(" "));
+};
+
+$('#generatemodel').on('click',function() {
+  // Check what is set to be true by the user
+  if($('#gr-true').is(':checked')) {
+    generalknowledge.push("gr");
   } else {
-    $('#agent1').html('Test');
+    var index = generalknowledge.indexOf("gr");
+    if(index > -1 ) {
+      generalknowledge.splice(index,1);
+    }
+  }
+  if($('#ir-true').is(':checked')) {
+    generalknowledge.push("ir");
+  } else {
+    var index = generalknowledge.indexOf("ir");
+    if(index > -1 ) {
+      generalknowledge.splice(index,1);
+    }
+  }
+  if($('#dr-true').is(':checked')) {
+    generalknowledge.push("dr");
+  } else {
+    var index = generalknowledge.indexOf("dr");
+    if(index > -1 ) {
+      generalknowledge.splice(index,1);
+    }
+  }
+  if($('#hd-true').is(':checked')) {
+    generalknowledge.push("hd");
+  } else {
+    var index = generalknowledge.indexOf("hd");
+    if(index > -1 ) {
+      generalknowledge.splice(index,1);
+    }
   }
 
-});
+  // Remove all duplicates from the array
+  var uniquegeneralknowledge = []
+  $.each(generalknowledge, function(i, el){
+    if($.inArray(el, uniquegeneralknowledge) === -1) uniquegeneralknowledge.push(el);
+  });
+  generalknowledge = uniquegeneralknowledge;
 
-$('#announcement2').on('click',function() {
-  $('#agent2').html('test2');
-});
+  // Debug
+  console.log(generalknowledge);
 
-$('#announcement3').on('click',function() {
-  $('#agent3').html('test3');
+  // Check implications of the given knowledge
+  checkImplications(generalknowledge);
+  generateModel(generalknowledge,'generalknowledge');
+  generateModel(belieflost,'agent1');
+  generateModel(beliefnormal,'agent2');
+  generateModel(beliefsusan,'agent3');
 });
